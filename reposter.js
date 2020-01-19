74c0be8e146068840f9e5bfdc6c95e37b2c4829d
nagraniadvbt2020 <60076886+nagraniadvbt2020@users.noreply.github.com>
2020-01-19 22:31:41 +0100

    Update reposter.js

--- reposter.js
+++ reposter.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("<SECRET_BOT_TOKEN>").catch(console.error);
+client.login("NjY3NzE1NTc5MzY4NDM5ODE4.XiTKjw.QpdPpYl88oYzBH4DJ9Ig0L3VnWE").catch(console.error);
 
 client.on("ready", function() {
 	client.user.setActivity(client.guilds.size + " server" + (client.guilds.size === 1 ? "" : "s"), { type: "WATCHING" }).catch(console.error);
